9735c91dacd02668d4b728d9b9290661694fd5c7
Jennifer Coulter <jennifer.coulter@cern.ch>
2015-07-29 22:25:45 +0200

    cleans up aftermath of batch jobs

---
+++ Batch_Scripts/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# A macro used to cleanup all the output from a set of batch jobs
+
+cd /afs/cern.ch/work/j/jcoulter/WORK/CMSSW_5_3_20/src/SummerRutgers15/
+
+mv *.log $PWD/Job_Output/Logs
+
+mv *output*.root $PWD/Job_Output/
+
+cd /afs/cern.ch/work/j/jcoulter/WORK/CMSSW_5_3_20/src/SummerRutgers15/Bash_Scripts/Batch_Scripts/
+
+mv LSF*/ ../../Job_Output/STDOUT
+
+
