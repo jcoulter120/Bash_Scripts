b17b308acade747baee5ea9ac53ada50c28bec23
Jennifer Coulter <jennifer.coulter@cern.ch>
2015-08-24 04:34:57 +0200

    update

---
+++ Batch_Scripts/PythiaSubmit.sh
@@ -0,0 +1,26 @@
+cd /afs/cern.ch/work/j/jcoulter/WORK/CMSSW_5_3_20/src/
+
+eval `scramv1 runtime -sh`
+
+cd /afs/cern.ch/work/j/jcoulter/WORK/CMSSW_5_3_20/src/GeneratorInterface
+
+echo "Pythia $jobNumber"
+#cd outputs/
+#medType="med1"
+mkdir $Pythia_{medType}_${jobNumber}_numEvents${nEvents}/
+cd $Pythia_{medType}_${jobNumber}_numEvents${nEvents}/
+#cp ../../GeneratorInterface/JewelInterface/test/medium-params.dat .
+#cp ../../GeneratorInterface/JewelInterface/test/splitint.dat .
+#cp ../../GeneratorInterface/JewelInterface/test/xsecs.dat .
+#cp ../../GeneratorInterface/JewelInterface/test/pdfs.dat .
+
+cmsRun ../../GeneratorInterface/JewelInterface/test/testPythiaZ2Star.py output="${low}_${high}_Pythia_$jobNumber" maxEvents=$nEvents ptHatLow=$low ptHatHigh=$high &> Pythia_${jobNumber}_numEvents${nEvents}.log
+#cp out.log ../LOG/${medType}_${jobNumber}_numEvents${nEvents}.log
+
+#these do nothing inf not Jewel
+#cp ${medType}_Pythia_${jobNumber}_numEvents${nEvents}.log ../LOG/.
+#cp out.hepmc ../HEPMC/${medType}_${jobNumber}_numEvents${nEvents}.hepmc
+#cp ${medType}_Pythia_${jobNumber}_numEvent${nEvents}.root ../ROOT/.
+cd /afs/cern.ch/work/j/jcoulter/WORK/CMSSW_5_3_20/src/SummerRutgers15/Bash_Scripts/Batch_Scripts/
+#rm ${jobNumber}_numEvents${nEvents}/ -rf
+#xrdcp JewelDijet_${jobNumber}_numEvent${nEvents}.root root://eosuser.cern.ch://eos/user/i/ihuntisa/
